cc58bafdb38f0130b2080134e12cfda933e8fdd1
Lee <171631592@qq.com>
2016-11-14 17:15:28 +0800

    create project

---
+++ app/src/main/java/com/cpacm/moemusic/moe_music1s/ExampleUnitText.java
@@ -0,0 +1,9 @@
+package com.cpacm.moemusic.moe_music1s;
+
+/**
+ * Created by DIY on 2016/11/14.
+ */
+
+
+public class ExampleUnitText {
+}
---
+++ core/src/main/java/com/cpacm/moemusic/core/ExampleUnitText.java
@@ -0,0 +1,19 @@
+package com.cpacm.moemusic.core;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by DIY on 2016/11/14.
+ */
+
+
+
+public class ExampleUnitText {
+    @Test
+    public void addition_isCorrect() throws Exception{
+        assertEquals(4,6+2);
+    }
+}
